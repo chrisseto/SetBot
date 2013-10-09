d2089eb2c9e9b602545e7a5263184b5814ae5d4c
chrisseto <chriskseto@gmail.com>
2013-10-09 00:25:03 -0400

    rearranged everything fucking circular includes

---
+++ src/config.c
@@ -0,0 +1,23 @@
+#include "../headers/config.h"
+const char* Server = "chat.freenode.net";
+//const int Port = 8000;
+const char* NickName = "SetBot";
+const char* User = "SetBot MintTop none Irobot";
+const char* Pass = "none";
+const char* Channel = "#stap-dev";
+const int DEBUG = 1;
+const extern char* ALLOWEDUSERS[];
+const char TRIGGER = '>';
+const char* COMMAND_STRINGS[] = {
+	"PRIVMSG", //0
+	"PASS", //1
+	"NICK", //2
+	"USER", //3
+	"PING", //4
+	"PONG", //5
+	"JOIN", //6
+	"QUIT" //7
+};
+const char* ALLOWEDUSERS[] = {
+	"SetBack"
+};
